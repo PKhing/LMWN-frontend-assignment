c1b1a51868a5ee6685d83b994f9e702c9ba9d0a8
PKhing <apimrak@gmail.com>
2022-01-08 17:59:31 +0700

    💄 Add Theme

---
+++ web/src/config/stitches.config.ts
@@ -0,0 +1,37 @@
+import {createStitches} from "@stitches/react";
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  theme: {
+    colors: {
+      primary: "#2C9BDB",
+      black400: "#BDBDBD",
+      black500: "#929292",
+      black600: "#828282",
+    },
+    fonts: {
+      NotoSans: "'Noto Sans Thai', sans-serif",
+    },
+  },
+  media: {
+    xs: "(max-width: 320px)",
+    sm: "(max-width: 496px)",
+    md: "(max-width: 768px)",
+    lg: "(max-width: 960px)",
+    xl: "(max-width: 1280px)",
+  },
+});
+
+globalCss({
+  "@import": [
+    "url('https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@300;400;500;600;700&display=swap')",
+  ],
+})();

b6555c70cc1d6d11624f1dbc029ffc37aa371ce2
PKhing <apimrak@gmail.com>
2022-01-09 18:11:52 +0700

    🥅 Handle API useTrips error

--- web/src/pages/landing/hooks/useTrips.tsx
+++ web/src/pages/landing/hooks/useTrips.tsx
@@ -1,3 +1,4 @@
+import {useError} from "common/components/Error/context/ErrorContext";
 import {ITrip} from "common/types";
 import {apiClient} from "config/axiosInstance";
 import {useEffect, useState} from "react";
@@ -6,16 +7,33 @@ import {useSearchParams} from "react-router-dom";
 const useTrips = () => {
   const [searchParams] = useSearchParams();
   const [trips, setTrips] = useState<ITrip[]>([]);
+  const {openError} = useError();
   useEffect(() => {
     const getTrips = async () => {
       console.log("get", searchParams.get("keyword"));
-      const res = await apiClient.get("/", {
-        params: {keyword: searchParams.get("keyword")},
-      });
-      setTrips(res.data);
+      try {
+        const res = await apiClient.get("/", {
+          params: {keyword: searchParams.get("keyword")},
+        });
+        setTrips(res.data);
+      } catch (err) {
+        const error = err as any;
+        if (!error.response) {
+          openError("Cannot connect to API");
+        } else {
+          openError(
+            `${error.response.status} : ${
+              error.response.data.message ||
+              error.response.statusText ||
+              "Error"
+            }`
+          );
+        }
+        console.log(error.response);
+      }
     };
     getTrips();
-  }, [searchParams]);
+  }, [openError, searchParams]);
 
   return trips;
 };
